0b446fe4c6887caee64a02daeb0353394e8ab5eb
Trapside <97183058+Trapside@users.noreply.github.com>
2026-01-31 04:15:38 +0200

    Delete worker.js

--- worker.js
+++
@@ -1,7 +0,0 @@
-import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm";
-
-// This handler listens for messages from the main thread (index.html)
-const handler = new WebWorkerMLCEngineHandler();
-self.onmessage = (msg) => {
-  handler.onmessage(msg);
-};
